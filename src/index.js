cded570cfd03ab1c2167365b38857c013d2827ca
DanH0210 <mnh02danh10@gmail.com>
2024-01-30 18:40:37 -0700

    Implement +-*/ functions

---
+++ src/index.js
@@ -0,0 +1,35 @@
+function main() {
+    const a = 5;
+    const b = 7;
+    const operator = '+';
+}
+
+function operate(operator, n1, n2) {
+    switch (operate) {
+        case '+':
+            return add(n1, n2);
+        case '-':
+            return subtract(n1, n2);
+        case '*':
+            return multiply(n1, n2);
+        case '/':
+            return divide(n1, n2);
+        default:
+            return "OOPS"
+    }
+}
+
+function add(n1, n2) {
+    return n1 + n2;
+}
+function subtract(n1, n2) {
+    return n1 - n2;
+}
+function multiply(n1, n2) {
+    return n1 * n2;
+}
+function divide(n1, n2) {
+    return n1 / n2;
+}
+
+main();
